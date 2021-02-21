b546bf39ec86590ce2c14254137107be941c4488
Will Nilges <wdn5796@rit.edu>
2021-02-20 23:26:49 -0500

    Okay Im not touching it anymore

--- squares.cpp
+++ squares.cpp
@@ -18,6 +18,7 @@ static double angle( Point pt1, Point pt2, Point pt0 )
     double dy2 = pt2.y - pt0.y;
     return (dx1*dx2 + dy1*dy2)/sqrt((dx1*dx1 + dy1*dy1)*(dx2*dx2 + dy2*dy2) + 1e-10);
 }
+
 // returns sequence of squares detected on the image.
 static void findSquares( const Mat& image, vector<vector<Point> >& squares )
 {
@@ -28,82 +29,47 @@ static void findSquares( const Mat& image, vector<vector<Point> >& squares )
     pyrUp(pyr, timg, image.size());
     vector<vector<Point> > contours;
     vector<Vec4i> hierarchy;
-    // find squares in every color plane of the image
-    // for( int c = 0; c < 3; c++ )
-    // {
-        int c = 0;
-        int ch[] = {c, 0};
-        mixChannels(&timg, 1, &gray0, 1, ch, 1);
-        // try several threshold levels
-        // for( int l = 0; l < N; l++ )
-        // {
-            // hack: use Canny instead of zero threshold level.
-            // Canny helps to catch squares with gradient shading
-            // if( l == 0 )
-            // {
-                // apply Canny. Take the upper threshold from slider
-                // and set the lower to 0 (which forces edges merging)
-                // Canny(gray0, gray, 0, thresh, 5);
-                // dilate canny output to remove potential
-                // holes between edge segments
-                // dilate(gray, gray, Mat(), Point(-1,-1));
-            // }
-            // else
-            // {
-            //     // apply threshold if l!=0:
-            //     //     tgray(x,y) = gray(x,y) < (l+1)*255/N ? 255 : 0
-                gray = gray0 >= (4+1)*255/N;
-            // }
-            // find contours and store them all as a list
-            findContours(gray, contours, hierarchy, RETR_CCOMP, CHAIN_APPROX_SIMPLE);
+    int c = 0;
+    int ch[] = {c, 0};
+    mixChannels(&timg, 1, &gray0, 1, ch, 1);
+    imwrite("tmp1.jpg", gray0);
+    gray = gray0 >= (4+1)*255/N;
+    // find contours and store them all as a list
+    findContours(gray, contours, hierarchy, RETR_CCOMP, CHAIN_APPROX_SIMPLE);
 
-            vector<Point> approx;
-            // test each contour
-            for( size_t i = 0; i < contours.size(); i++ )
+    vector<Point> approx;
+    // test each contour
+    for( size_t i = 0; i < contours.size(); i++ )
+    {
+        // approximate contour with accuracy proportional
+        // to the contour perimeter
+        approxPolyDP(contours[i], approx, arcLength(contours[i], true)*0.02, true);
+        // square contours should have 4 vertices after approximation
+        // relatively large area (to filter out noisy contours)
+        // and be convex.
+        // Note: absolute value of an area is used because
+        // area may be positive or negative - in accordance with the
+        // contour orientation
+        if( approx.size() == 4 &&
+            fabs(contourArea(approx)) > 1000 &&
+            isContourConvex(approx) )
+        {
+            double maxCosine = 0;
+            for( int j = 2; j < 5; j++ )
             {
-                // approximate contour with accuracy proportional
-                // to the contour perimeter
-                approxPolyDP(contours[i], approx, arcLength(contours[i], true)*0.02, true);
-                // square contours should have 4 vertices after approximation
-                // relatively large area (to filter out noisy contours)
-                // and be convex.
-                // Note: absolute value of an area is used because
-                // area may be positive or negative - in accordance with the
-                // contour orientation
-                if( approx.size() == 4 &&
-                    fabs(contourArea(approx)) > 1000 &&
-                    isContourConvex(approx) )
-                {
-                    double maxCosine = 0;
-                    for( int j = 2; j < 5; j++ )
-                    {
-                        // find the maximum cosine of the angle between joint edges
-                        double cosine = fabs(angle(approx[j%4], approx[j-2], approx[j-1]));
-                        maxCosine = MAX(maxCosine, cosine);
-                    }
-                    // if cosines of all angles are small
-                    // (all angles are ~90 degree) then write quandrange
-                    // vertices to resultant sequence
-                    if( maxCosine < 0.1 && hierarchy[i][3] >= 0 )
-                        // if (squares.size() > 0){
-                            // for ( vector<Point> square : squares ) {
-                                // std::cout << "drop.\n";
-                                // Make sure we're only taking one square from a particular shape (don't ask).
-                                // cout << std::abs(square.at(0).x - approx.at(0).x) << " , " << std::abs(square.at(0).y - approx.at(0).y) << "\n";
-                                // if (std::abs(square.at(0).x - approx.at(0).x) > 1500 && std::abs(square.at(0).y - approx.at(0).y) > 1500) {
-                                    // std::cout << "Adding.\n";
-                                    // squares.push_back(approx);
-                                    // break;
-                                // }
-                            // }
-                        // } else {
-                            squares.push_back(approx);
-                        // }
-                }
+                // find the maximum cosine of the angle between joint edges
+                double cosine = fabs(angle(approx[j%4], approx[j-2], approx[j-1]));
+                maxCosine = MAX(maxCosine, cosine);
             }
-        // }
-    // }
+            // if cosines of all angles are small
+            // (all angles are ~90 degree) then write quandrange
+            // vertices to resultant sequence
+            if( maxCosine < 0.1 && hierarchy[i][3] >= 0 )
+                    squares.push_back(approx);
+        }
+    }
 }
+
 // the function draws all the squares in the image
 static void drawSquares( Mat& image, const vector<vector<Point> >& squares )
 {
